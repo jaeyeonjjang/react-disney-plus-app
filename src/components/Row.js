0ab395e883bc3180488283a95b8c498f99aa8919
LAPTOP-HSFQK3VH\wodus <wodus0936@gmail.com>
2024-07-01 17:13:40 +0900

    row swiper 기능 추가

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,6 +2,16 @@ import React, {useCallback, useEffect, useState} from 'react'
 import axios from '../api/axios'
 import "./Row.css"
 import MovieModal from './MovieModal'
+import styled from 'styled-components'
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import "swiper/css"
+// import "swiper/css/navigtion";
+// import "swiper/css/scrollbar";
+// import "swiper/css/pagination";
+import "swiper/swiper-bundle.css";
 
 const Row = ({title, id, fetchUrl}) => {
 
@@ -25,47 +35,97 @@ const Row = ({title, id, fetchUrl}) => {
 
 
   return (
-    <div>
+    <Container>
       <h2>{title}</h2>
-      <div className='slider'>
-        <div className='slider_arrow-left'>
-          <span className='arrow'
-           onClick={() => {
-            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-           }}>
-            {"<"}
-          </span>
-        </div>
-        <div id={id} className='row_posters'>
-          {movies.map(movie => (
-            <img
-              key={movie.id}
-              className='row_poster'
-              alt={movie.name}
-              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-              onClick={() => handleClick(movie)}
-              />
-          ))}s
-        </div>
-        <div className='slider_arrow-right'>
-          <span className='arrow'
-          onClick={() => {
-            document.getElementById(id).scrollLeft += window.innerWidth - 80;
-          }}>
-            {">"}
-          </span>
-        </div>
-      </div>
+      <Swiper
+        // install Swiper Modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}  // loop 기능을 사용할 것인지
+        navigation // arrow 버튼 사용 유무
+        pagination={{clickable: true,}} // 페이지 버튼 보이게 할지
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidePerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidePerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidePerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidePerGroup: 3,
+          }
+        }}
+      >
+        <Content id={id}>
+        {movies.map(movie => (
+            <SwiperSlide>
+              <Wrap>
+              <img
+                key={movie.id}
+                alt={movie.name}
+                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                onClick={() => handleClick(movie)}
+                />
+              </Wrap>
+            </SwiperSlide>
+          ))}
+        </Content>
+      </Swiper>
 
-       {modalOpen && 
-        <MovieModal
-        {...movieSelected}
-        setModalOpen = {setModalOpen}
-        />
+        {modalOpen && 
+          <MovieModal
+          {...movieSelected}
+          setModalOpen = {setModalOpen}
+          />
 
-      } 
-    </div>
+        } 
+      </Container>
   )
 }
 
 export default Row
+
+const Container = styled.div`
+  padding : 0 0 26px;
+`;
+
+const Content = styled.div`
+`;
+
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+              rgb(0 0 0/73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index:1;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
